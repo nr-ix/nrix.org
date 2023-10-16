1693038a95e7834331ef0aeb487f579edb9fe47c
smitug01 <sumily20@gmail.com>
2023-10-16 17:29:27 +0800

    Initial commit

---
+++ app.config.ts
@@ -0,0 +1,32 @@
+export default defineAppConfig({
+  docus: {
+    title: 'NiceRoute Internet Exchange',
+    description: 'This page could solve your most of the problems.',
+    image: 'https://m4nic-docs.pages.dev/img/static-image.png',
+    socials: {
+      // twitter: 'nuxtstudio',
+      github: 'nr-ix'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true
+    },
+    footer: {
+      // iconLinks: [
+      //   {
+      //     href: 'https://nuxt.com',
+      //     icon: 'simple-icons:nuxtdotjs'
+      //   }
+      // ],
+      textLinks: [
+        {
+          text: '2023 - 2023 NiceRoute Internet Exchange | Powered by 404 Network Information',
+          href: ''
+        }
+      ]
+    }
+  }
+})
