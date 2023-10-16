e7aa57d65388b462f46f27cf90b20b1edd74c407
smitug01 <sumily20@gmail.com>
2023-10-16 18:17:38 +0800

    Rollback Logo.vue

---
+++ components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+    <span style="font-weight: 600; font-size: 1rem; line-height: 1.5rem;">
+        NiceRoute Internet Exchanges
+    </span>
+    <br />
+    <span style="font-weight: 600; font-size: 1.4rem; line-height: 1.75rem; margin-top: 2rem; margin-button: 2rem">
+        Official Page
+    </span>
+</template>
+
+<style lang="ts" scoped>
+    css({
+    svg: {
+        color: '{color.gray.900}',
+        height: 'inherit',
+        '@dark': {
+        color: '{color.gray.100}'
+        },
+    }
+    })
+</style>
